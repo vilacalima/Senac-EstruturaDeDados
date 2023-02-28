48f35c5cb71ce61a22381e18047b4e4f566e9119
ROBSON VILACA LIMA <robson.vlima3@CAS474765C148A.local>
2023-02-28 19:54:14 -0300

    Teste Commit

---
+++ src/Aula2/cadastro/Array.java
@@ -0,0 +1,5 @@
+package Aula2.cadastro;
+
+public class Array {
+
+}
---
+++ src/Aula2/cadastro/Pessoa.java
@@ -0,0 +1,27 @@
+package Aula2.cadastro;
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	private int id;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
